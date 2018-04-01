bd1a85acc211fb6a79f4377735beadd3c23f1223
Ivo Georgiev <ivo@linvo.org>
2018-04-02 01:44:05 +0300

    add forgotten dir

---
+++ build/browserify-client-deps.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+var browserify = require('browserify')
+var fs = require('fs')
+var babelify = require('babelify')
+var Readable = require('stream').Readable
+var pkg = require('../package')
+
+module.exports = function() {
+	var opts = { 
+		insertGlobalVars: true,
+		basedir: process.cwd(),
+		builtins: [
+			'_process', 
+			'util', 'buffer', 'url', 
+			'punycode', // ??
+			'querystring', 'os', 'path', 
+			//'crypto',
+			//'stream', 'events', 'string_decoder', 'assert'
+			'events',
+		] 
+	}
+
+	var b = browserify([ ], opts);
+
+
+	// basic list, we'll extend it when reading resources.jade
+	// cat src/resources.pug | grep script | grep node_modules | cut -d '/' -f2 | while read line; do echo "'$line': 1,"; done | sort | uniq
+	var ignoreList = {
+		'angular': 1,
+		'angular-ui-router': 1,
+		'jquery': 1,
+		'moment': 1,
+		'velocity-animate': 1,
+		'popper.js': 1,
+		'bootstrap': 1,
+	}
+	
+
+	var pkgFile = new Readable();
+	// displayName, version, name, website is all we need; hash is needed only when compiling the HTML
+	pkgFile.push('module.exports = '+JSON.stringify({ displayName: pkg.displayName, name: pkg.name, version: pkg.version, website: pkg.website }));
+	pkgFile.push(null);
+	b.require(pkgFile, { expose: './package' });
+
+	b.require('babel-polyfill');
+ 	
+ 	// Do we need those?
+	b.require('url');
+	b.require('os');
+	b.require('path');
+	b.require('querystring');
+	b.require('buffer');
+
+	//b.require(config, { expose: 'stremio-config' })
+
+	for (dep in require('../package').dependencies) {
+		if (!ignoreList[dep]) { 
+			//console.log('including '+dep)
+			b.require(dep);
+
+		}
+	}
+
+	return b.transform(babelify, { presets: ['latest'], plugins: ['transform-runtime']})
+}
---
+++ build/dev-server.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+var pkg = require('../package')
+
+// Serve development assets - compile jade/stylus on demand
+var express = require('express')
+
+var pug = require('pug')
+var stylus = require('stylus')
+var nib = require('nib')
+var fs = require('fs')
+var url = require('url')
+var depsBlob = require('./browserify-client-deps')
+var router = express.Router()
+
+router.get('/', function(req, res) {
+    var u = url.parse(req.url, true);
+    fs.readFile('./src/index.pug', function(err, template) {
+        if (err) { console.error(err); res.writeHead(500); return res.end(); }
+        res.end(pug.render(template, { mode: u.query.mode || 'web', filename: './src/index.pug', pkg: pkg }));
+    });
+});
+
+router.get(/^\/css\/(.*).css/, function(req, res, next) {
+    var pathname = url.parse(req.url).pathname.slice('/css/'.length);
+    var styl_file = pathname.replace('.css', '.styl');
+    var p = (pathname.indexOf('/') === -1 ? './src/styl/' : './') + styl_file; // put /styl in front if it's top-level
+    fs.readFile(p, function(err, str) {
+        if (err) { console.error(err); return next(); }
+        stylus(str.toString()).set('filename', p).use(nib()).render(serveCss.bind(null, req, res));
+    });
+});
+function serveCss(req, res, err, stylesheets) {
+    if (err) { console.error(err); res.writeHead(500); return res.end(); }
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.end(stylesheets);
+}
+
+router.get('/depsblob.js', function(req, res) {
+    depsBlob().bundle().pipe(res);
+});
+
+router.use(require('serve-static')('./'));
+
+// Listen
+var app = express()
+var server = app.listen(8095)
+
+app.use(router)
+
+server.on('listening', function() {
+     console.log('Development server listening on: '+server.address().port)
+})
